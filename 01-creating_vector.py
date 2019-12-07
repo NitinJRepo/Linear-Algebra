0a75cbd4dcdbaea199128bbe50d28d5572d0edbd
NitinJRepo <nitin.jatpuriya@gmail.com>
2019-12-07 09:05:49 +0530

    Added vector program

---
+++ 01-creating_vector.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+# Create a vector as a row
+vector_row = np.array([1, 2, 3])
+
+print("row vector: ", vector_row)
+
+# Create a vector as a column
+vector_column = np.array([[1],
+[2],
+[3]])
+
+print("column vector: ", vector_column)
